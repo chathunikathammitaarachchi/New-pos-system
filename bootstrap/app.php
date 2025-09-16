0be181d35fb43fdf4e8aab3adb0692b7004fb582
chathunika dayangani thammitaarachci <chathunikathammitaarachchi74@gmail.com>
2025-09-16 16:51:28 +0530

    add

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,6 +20,8 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleAppearance::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
+            \App\Http\Middleware\HandleInertiaRequests::class, 
+
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
