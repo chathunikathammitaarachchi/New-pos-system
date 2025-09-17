ae2774682a4f5901fd296eeba7da748fd84d69c3
chathunika dayangani thammitaarachci <chathunikathammitaarachchi74@gmail.com>
2025-09-17 12:42:58 +0530

    add

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,9 +1,9 @@
 import '../css/app.css';
-
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
+
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -20,6 +20,7 @@ createInertiaApp({
         color: '#4B5563',
     },
 });
+
 
 
 initializeTheme();
