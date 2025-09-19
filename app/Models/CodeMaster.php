5fa3002fd734626b224567f7f282e5fbcf7a1059
chathunika dayangani thammitaarachci <chathunikathammitaarachchi74@gmail.com>
2025-09-19 13:54:41 +0530

    add

--- app/Models/CodeMaster.php
+++ app/Models/CodeMaster.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class CodeMaster extends Model
 {
     protected $table = 'code_master';
-    protected $fillable = ['conkey', 'concode', 'cdcode', 'cdname'];
+    protected $fillable = ['conkey', 'concode', 'catkey', 'cname'];
     public $timestamps = true;
 }
