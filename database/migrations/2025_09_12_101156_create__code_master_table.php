8f28ebcacc9d59aa3ed6d823090599aebdc4427c
chathunika dayangani thammitaarachci <chathunikathammitaarachchi74@gmail.com>
2025-09-19 13:54:51 +0530

    add

--- database/migrations/2025_09_12_101156_create__code_master_table.php
+++ database/migrations/2025_09_12_101156_create__code_master_table.php
@@ -4,28 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-  // code_master migration
-Schema::create('code_master', function (Blueprint $table) {
-    $table->id(); 
-    $table->string('conkey');
-    $table->string('concode');
-  $table->string('cdcode')->unique();
-      $table->string('cdname')->unique();
-    $table->timestamps();
-});
-
-
+return new class extends Migration {
+    public function up(): void {
+        Schema::create('code_master', function (Blueprint $table) {
+            $table->id();
+            $table->string('conkey');
+            $table->string('concode');
+            $table->string('catkey')->unique();
+            $table->string('cname')->unique();
+            $table->timestamps();
+        });
     }
-public function down(): void
-{
-    Schema::dropIfExists('code_master'); // Correct table name here!
-}
 
+    public function down(): void {
+        Schema::dropIfExists('code_master');
+    }
 };
